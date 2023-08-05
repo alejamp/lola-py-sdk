5a9ae017daceed292c564564b2a8e93e96f5c989
Alejandro Pirola <alejamp@gmail.com>
2023-08-05 18:37:54 -0300

    add send image to ctx.messager

--- lolapy/lola_message_sender.py
+++ lolapy/lola_message_sender.py
@@ -22,6 +22,22 @@ class LolaMessageSender:
         response.raise_for_status()
         return
     
+    def send_image_message(self, url, text=None, appendToHistory=False, isPrivate=False):
+        url = f'{self.prompter_url}/api/messanger/send/photo'
+        headers = {'x-lola-auth': self.lola_token, 'Content-Type': 'application/json'}
+        data = {
+            'lead': self.lead, 
+            'text': text,
+            'attachment': { 
+                'url': url
+            },
+            'appendToHistory': appendToHistory,
+            'isPrivate': isPrivate
+        }
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()
+        return
+    
     def send_typing_action(self):
         url = f'{self.prompter_url}/api/messanger/send/action'
         headers = {'x-lola-auth': self.lola_token, 'Content-Type': 'application/json'}
